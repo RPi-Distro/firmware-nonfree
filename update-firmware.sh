b4db58922e6ad9d4cae7914dda60c2a3682c2930
Frederik Schüler <fs@debian.org>
2006-09-05 15:37:13 +0000

    Prepare for release
    svn path=/dists/trunk/firmware-nonfree/; revision=7343

--- update-firmware.sh
+++ update-firmware.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-wget -r -nH -N --cut-dirs=3 -P qlogic-fw ftp://ftp.qlogic.com/outgoing/linux/firmware/
+wget -r -nH -N --cut-dirs=3 -P qlogic ftp://ftp.qlogic.com/outgoing/linux/firmware/
