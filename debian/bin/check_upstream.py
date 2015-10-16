d599c6ca3fc04c6a7539b7c9e2cbade63ddea51b
Ben Hutchings <ben@decadent.org.uk>
2015-10-16 19:23:19 +0100

    check_upstream.py: Split license check into multiple functions

--- debian/bin/check_upstream.py
+++ debian/bin/check_upstream.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import errno, filecmp, fnmatch, glob, os.path, re, sys
+from enum import Enum
 rules_defs = dict((match.group(1), match.group(2))
                   for line in open('debian/rules.defs')
                   for match in [re.match(r'(\w+)\s*:=\s*(.*)\n', line)])
@@ -8,6 +9,41 @@ sys.path.append('/usr/share/linux-support-%s/lib/python' %
                 rules_defs['KERNELVERSION'])
 from debian_linux.firmware import FirmwareWhence
 from debian_linux.config import ConfigParser, SchemaItemList
+
+class DistState(Enum):
+    undistributable = 1
+    non_free = 2
+    free = 3
+
+def is_source_available(section):
+    for file_info in section.files.values():
+        if not (file_info.source or file_info.binary.endswith('.cis')):
+            return False
+    return True
+
+def check_section(section):
+    if re.search(r'^BSD\b'
+                 r'|^GPLv2 or OpenIB\.org BSD\b'
+                 r'|\bPermission\s+is\s+hereby\s+granted\s+for\s+the\s+'
+                 r'distribution\s+of\s+this\s+firmware\s+(?:data|image)\b'
+                 r'(?!\s+as\s+part\s+of)'
+                 r'|\bRedistribution\s+and\s+use\s+in(?:\s+source\s+and)?'
+                 r'\s+binary\s+forms\b'
+                 r'|\bPermission\s+is\s+hereby\s+granted\b[^.]+\sto'
+                 r'\s+deal\s+in\s+the\s+Software\s+without'
+                 r'\s+restriction\b'
+                 r'|\bredistributable\s+in\s+binary\s+form\b',
+                 section.licence):
+        return (DistState.free if is_source_available(section)
+                else DistState.non_free)
+    elif re.match(r'^(?:D|Red)istributable\b', section.licence):
+        return DistState.non_free
+    elif re.match(r'^GPL(?:v2|\+)?\b', section.licence):
+        return (DistState.free if is_source_available(section)
+                else DistState.undistributable)
+    else:
+        # Unrecognised and probably undistributable
+        return DistState.undistributable
 
 def main(source_dir):
     config = ConfigParser({
@@ -19,38 +55,11 @@ def main(source_dir):
     exclusions = config['upstream',]['exclude']
 
     for section in FirmwareWhence(open(os.path.join(source_dir, 'WHENCE'))):
-        if re.search(r'^BSD\b'
-                     r'|^GPLv2 or OpenIB\.org BSD\b'
-                     r'|\bPermission\s+is\s+hereby\s+granted\s+for\s+the\s+'
-                     r'distribution\s+of\s+this\s+firmware\s+(?:data|image)\b'
-                     r'(?!\s+as\s+part\s+of)'
-                     r'|\bRedistribution\s+and\s+use\s+in(?:\s+source\s+and)?'
-                     r'\s+binary\s+forms\b'
-                     r'|\bPermission\s+is\s+hereby\s+granted\b[^.]+\sto'
-                     r'\s+deal\s+in\s+the\s+Software\s+without'
-                     r'\s+restriction\b'
-                     r'|\bredistributable\s+in\s+binary\s+form\b',
-                     section.licence):
-            # Suitable for main if source is available; non-free otherwise
-            maybe_free = True
-            pass
-        elif re.match(r'^(?:D|Red)istributable\b', section.licence):
-            # Only suitable for non-free
-            maybe_free = False
-            pass
-        elif re.match(r'^GPL(?:v2|\+)?\b', section.licence):
-            # Suitable for main if source is available; not distributable
-            # otherwise
-            continue
-        else:
-            # Probably not distributable
-            continue
-        for file_info in section.files.values():
-            if (not (maybe_free and
-                     (file_info.source or file_info.binary.endswith('.cis')))
-                and not any(fnmatch.fnmatch(file_info.binary, exclusion)
-                            for exclusion in exclusions)):
-                update_file(source_dir, dest_dirs, file_info.binary)
+        if check_section(section) == DistState.non_free:
+            for file_info in section.files.values():
+                if not any(fnmatch.fnmatch(file_info.binary, exclusion)
+                           for exclusion in exclusions):
+                    update_file(source_dir, dest_dirs, file_info.binary)
 
 def update_file(source_dir, dest_dirs, filename):
     source_file = os.path.join(source_dir, filename)
